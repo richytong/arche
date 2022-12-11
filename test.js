4486685d7c3ecb693a4e4970d9798f2cfa759166
Richard <richytong@gmail.com>
2022-12-10 22:11:03 -0800

    styled option enables the css prop

--- test.js
+++ test.js
@@ -34,7 +34,7 @@ describe('Arche', () => {
           Article('yo'),
         ]),
       ])
-      // console.log(JSON.stringify(el))
+
       assert.strictEqual(
         JSON.stringify(el),
         '{"type":"div","children":[{"type":"h1","children":[{"type":"text","text":"header"}],"style":{}},{"type":"p","children":[{"type":"text","text":"description"}],"style":{"color":"grey"}},{"type":"span","children":[],"style":{},"id":"hey","excluded":null},{"type":"div","children":[{"type":"article","children":[{"type":"text","text":"yo"}],"style":{}}],"style":{},"id":"nested"}],"style":{}}')
@@ -59,10 +59,63 @@ describe('Arche', () => {
           Article('yo'),
         ]),
       ])
-      // console.log(JSON.stringify(el))
+
       assert.strictEqual(
         JSON.stringify(el),
         '["div",{},[["h1",{},["header"]],["p",{"style":{"color":"grey"}},["description"]],["span",{"id":"hey","excluded":null},[]],["div",{"id":"nested"},[["article",{},["yo"]]]]]]')
+    })
+  })
+
+  describe('styled 3-ary creator.createElement', () => {
+    const mockReact = {
+      createElement(type, props, ...children) {
+        return [type, props || {}, children || []]
+      },
+    }
+
+    const identity = value => value
+
+    const styled = {
+      h1: identity,
+      h2: identity,
+      div: identity,
+      p: identity,
+      b: identity,
+      span: identity,
+      article: identity,
+    }
+
+    const rootElement = Arche(mockReact, { styled })
+    const { Div, H1, P, Span, Article } = rootElement
+
+    it('tree structure 1', () => {
+      const el = Div([
+        H1({ css: 'h2' }, 'header'), // should swap with h2
+        P({ style: { color: 'grey' } }, 'description'),
+        Span({ id: 'hey', excluded: null }),
+        Div({ id: 'nested' }, [
+          Article('yo'),
+        ]),
+      ])
+
+      assert.strictEqual(
+        JSON.stringify(el),
+        '["div",{},[["h2",{},["header"]],["p",{"style":{"color":"grey"}},["description"]],["span",{"id":"hey","excluded":null},[]],["div",{"id":"nested"},[["article",{},["yo"]]]]]]')
+    })
+
+    it('tree structure 2', () => {
+      const el = Div([
+        H1('header'),
+        P({ css: 'h3', style: { color: 'grey' } }, 'description'),
+        Span({ css: 'b', id: 'hey', excluded: null }),
+        Div({ css: 'article', id: 'nested' }, [
+          Article('yo'),
+        ]),
+      ])
+
+      assert.strictEqual(
+        JSON.stringify(el),
+        '["div",{},[["h1",{},["header"]],["h3",{"style":{"color":"grey"}},["description"]],["b",{"id":"hey","excluded":null},[]],["article",{"id":"nested"},[["article",{},["yo"]]]]]]')
     })
   })
 })
